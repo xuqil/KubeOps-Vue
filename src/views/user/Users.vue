40e60187b03c070c6c8273338fd22981de59e7cd
xuqil <13829842679@163.com>
2020-03-16 15:27:16 +0800

    实现用户列表分页

--- src/views/user/Users.vue
+++ src/views/user/Users.vue
@@ -10,24 +10,48 @@
     <el-card class="box-card">
       <!--            搜索与添加区域-->
       <el-row :gutter="20">
-        <el-col :span="8">
-          <el-input placeholder="请输入内容" >
-            <el-button slot="append" icon="el-icon-search"></el-button>
-          </el-input>
-        </el-col>
+<!--        <el-col :span="8">-->
+<!--          <el-input placeholder="请输入内容">-->
+<!--            <el-button slot="append" icon="el-icon-search"></el-button>-->
+<!--          </el-input>-->
+<!--        </el-col>-->
         <el-col :span="4">
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
-                  用户列表区
+      <!--用户列表区-->
       <el-table :data="userList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
         <el-table-column label="姓名" prop="username"></el-table-column>
         <el-table-column label="邮箱" prop="email"></el-table-column>
         <el-table-column label="电话" prop="mobile"></el-table-column>
         <el-table-column label="角色" prop="roles"></el-table-column>
-        <el-table-column label="状态" prop="active"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.active">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
+      <!--分页区域-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.page"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -35,7 +59,7 @@
 <script>
   export default {
     name: "User",
-    data(){
+    data() {
       return {
         queryInfo: {
           // 当前页数
@@ -51,15 +75,24 @@
       this.getUserList()
     },
     methods: {
-       getUserList(){
-       this.$api.users(this.queryInfo).then( res => {
-         this.userList = res.data.results;
-         console.log(this.userList);
-         this.total = res.data.count;
-       }).catch( onerror => {
-         console.log(onerror);
-         return this.$message.error('获取用户列表失败');
-       })
+      getUserList() {
+        this.$api.users(this.queryInfo).then(res => {
+          this.userList = res.data.results;
+          console.log(this.userList);
+          this.total = res.data.count;
+        }).catch(onerror => {
+          console.log(onerror);
+          return this.$message.error('获取用户列表失败');
+        })
+      },
+      //分页
+      handleSizeChange(newSize) {
+        this.queryInfo.page_size = newSize;
+        this.getUserList()
+      },
+      handleCurrentChange(newPage) {
+        this.queryInfo.page = newPage;
+        this.getUserList()
       }
     }
   }
