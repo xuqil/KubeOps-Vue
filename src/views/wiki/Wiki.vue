e941fb93bf8f60935eb0e6e792cfc66d85386223
xuqil <13829842679@163.com>
2020-04-03 17:39:15 +0800

    添加文章删除功能

--- src/views/wiki/Wiki.vue
+++ src/views/wiki/Wiki.vue
@@ -5,7 +5,6 @@
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>wiki</el-breadcrumb-item>
     </el-breadcrumb>
-    <!--    <el-card>-->
     <el-row :gutter="20">
       <!--文章-->
       <el-col :span="16" v-if="!showPostDetailVisible">
@@ -16,6 +15,7 @@
               <!--标题--->
               <div class="post_title">{{items.title}}</div>
               <div class="post_title_bottom">
+                <span class="post_author"><i class="el-icon-user"></i>: {{items.author.username}}</span>
                 <span class="post_category">分类: {{items.category.name}}</span>
                 <span>
                     <i class="el-icon-collection-tag"></i>:
@@ -42,7 +42,8 @@
             <!--标题--->
             <div class="post_title">{{detailPostForm.title}}</div>
             <div class="post_title_bottom">
-                <span>
+              <span class="post_author"><i class="el-icon-user"></i>: {{detailPostForm.author.username}}</span>
+              <span>
                   <i class="el-icon-collection-tag"></i>:
                   <el-tag :type="tagsColors[i]" size="mini" v-for="(tag, i) in detailPostForm.tags" :key="tag.id">{{tag.name}}
                   </el-tag>
@@ -53,7 +54,7 @@
               <div v-html="detailPostForm.body"></div>
             </div>
             <div class="post_operation">
-              <el-button type="text" icon="el-icon-edit" @click="showPostDetail(detailPostForm.id)">删除</el-button>
+              <el-button type="text" icon="el-icon-edit" @click="deletePost(detailPostForm.id)">删除</el-button>
               <el-button type="text" icon="el-icon-delete" @click="showPostDetail(detailPostForm.id)">编辑</el-button>
             </div>
             <div class="post_time">{{detailPostForm.c_time | dataFormat}}</div>
@@ -78,9 +79,6 @@
         </div>
       </el-col>
     </el-row>
-
-
-    <!--    </el-card>-->
   </div>
 </template>
 
@@ -135,9 +133,31 @@
         this.detailPostForm = this.postsList.find(function (obj) {
           return obj.id === id
         });
-        console.log(this.detailPostForm)
         this.showPostDetailVisible = true
       },
+      //删除文章
+      async deletePost(id) {
+        const confirmResult = await this.$confirm(
+          '此操作将永久删除该文章, 是否继续?',
+          '提示',
+          {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }
+        ).catch(err => err);
+        if (confirmResult !== 'confirm') {
+          return this.$message.info('已取消删除')
+        }
+        this.$api.postDelete(id).then(res => {
+          this.$message.success('删除成功！');
+          this.getPostsList()
+          this.showPostDetailVisible = false;
+        }).catch(onerror => {
+          console.log(onerror);
+          return this.$message.error('删除失败！')
+        });
+      }
     },
   }
 </script>
@@ -163,12 +183,12 @@
     margin-bottom: 30px;
   }
 
-  .post_category {
+  .post_author, .post_category {
     margin-right: 10px
   }
 
   .post_excerpt {
-    text-align: center;
+    width: 890px;
   }
 
   .post_body {
